ebccad2e13f8b2c044d5320e9b84f08d97ec87f6
Jeremy Parker <jrobinparker@gmail.com>
2018-07-08 04:21:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
 
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
